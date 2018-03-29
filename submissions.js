d960a463f8eda989e616c0f5217a4eaf200292e3
Debarshi-Choudhury <debarshi.choudhury1@gmail.com>
2018-03-30 04:03:29 +0530

    bug fix

--- submissions.js
+++ submissions.js
@@ -1,7 +1,5 @@
 const request=require('request');
 const cheerio=require('cheerio');
-const pageCount=require('./pageCount');
-
 
 var submissionsFunc=function(usr,cb){
 
@@ -20,89 +18,103 @@ var submissionsFunc=function(usr,cb){
 	var mem_lim_exceed=0;
 	var hacked=0;
 	var compilation_error=0;
+	var message='all submission pages taken';
 
-	pageCount.pageCountFunc(url,function(error,pages){
-		if(error){
-			console.log(error);
-		}else{
-			// console.log(pages);
-			var page_number= 1;
-			function callNext() {
-			   if (page_number>pages) {
-					requestEnded();
-			   }else {
-					page_number++;
-			   	request(`http://codeforces.com/submissions/${usr}/page/${page_number-1}`, function(err,resp,body) {
-							if(err){
-								console.log(`Error while retrieving submission page number ${page_number-1}`);
+	var pages=1;
+	var page_number= 1;
+	var pages_available=false;
+
+	
+	
+	function callNext() {
+		if (page_number>pages) {
+			requestEnded();
+		}else {
+			page_number++;
+	   	request(`http://codeforces.com/submissions/${usr}/page/${page_number-1}`, function(err,resp,body) {
+					if(err){
+						console.log(`Error while retrieving submission page number ${page_number-1}`);
+					}
+					else{
+						var $=cheerio.load(body);
+
+						if(!pages_available){
+							var totalpages=$('div.pagination ul').children('li').eq(-2).text();
+							totalpages=parseInt(totalpages);
+							if(isNaN(totalpages)){
+								totalpages=1;
 							}
-							else{
-								var $=cheerio.load(body);
-
-								$('table.status-frame-datatable tr').each(function(){
-									curr_class=$(this).attr('class');
-									if(curr_class!=='first-row'){
-										verdict=$(this).children('td').eq(5).text().trim();
-										ques_name=$(this).children('td').eq(3).text();
-										pos=ques_name.search('-');
-										ques_name=ques_name.substring(pos+1).trim();
-
-										verdict=verdict.split(' ');
-										verdict=verdict[0].trim();
-
-
-										if(verdict==='Accepted'){
-											accepted++;
-											uniq_ques_solved[ques_name]=1;
-										}else if(verdict==='Wrong'){
-											wrong_answer++;
-										}else if(verdict==='Time'){
-											time_lim_exceed++;
-										}else if(verdict==='Runtime'){
-											runtime_error++;
-										}else if(verdict==='Memory'){
-											mem_lim_exceed++;
-										}else if(verdict==='Compilation'){
-											compilation_error++;
-										}else if(verdict==='Hacked'){
-											hacked++;
-										}
-
-										// console.log(ques_name);
-										// console.log(verdict);
-									}
-								});
+							pages=totalpages;
+							pages_available=true;
+							if(pages>30){
+								pages=30;
+								msg='Only top 30 submission pages taken';
 							}
-			            // Call the next request inside the callback, so we are sure that the
-							//next request is ran just after this request has ended
-			            callNext();
-			   	});
-				}
-			}
+						}
 
-			//Here we will call the callNext function for the first time
-			callNext();
+						$('table.status-frame-datatable tr').each(function(){
+							curr_class=$(this).attr('class');
+							if(curr_class!=='first-row'){
+								verdict=$(this).children('td').eq(5).text().trim();
+								ques_name=$(this).children('td').eq(3).text();
+								pos=ques_name.search('-');
+								ques_name=ques_name.substring(pos+1).trim();
+
+								verdict=verdict.split(' ');
+								verdict=verdict[0].trim();
 
 
-			function requestEnded(){
-			    // console.log("All Request's have ended");
-				 uniq_ques_solved=Object.keys(uniq_ques_solved);
-				 var submissionsData={
-			 		uniq_ques_solved,
-			 		no_of_uniq_ques_solved:uniq_ques_solved.length,
-			 		accepted,
-			 		wrong_answer,
-			 		time_lim_exceed,
-			 		runtime_error,
-			 		mem_lim_exceed,
-			 		hacked,
-			 		compilation_error,
-			 	};
-				// console.log(submissionsData);
-				cb(null,submissionsData);
-			}
+								if(verdict==='Accepted'){
+									accepted++;
+									uniq_ques_solved[ques_name]=1;
+								}else if(verdict==='Wrong'){
+									wrong_answer++;
+								}else if(verdict==='Time'){
+									time_lim_exceed++;
+								}else if(verdict==='Runtime'){
+									runtime_error++;
+								}else if(verdict==='Memory'){
+									mem_lim_exceed++;
+								}else if(verdict==='Compilation'){
+									compilation_error++;
+								}else if(verdict==='Hacked'){
+									hacked++;
+								}
+
+								// console.log(ques_name);
+								// console.log(verdict);
+							}
+						});
+					}
+	            // Call the next request inside the callback, so we are sure that the
+					//next request is ran just after this request has ended
+	            callNext();
+	   	});
 		}
-	});
+	}
+
+	//Here we will call the callNext function for the first time
+	callNext();
+
+
+	function requestEnded(){
+	    // console.log("All Request's have ended");
+		 uniq_ques_solved=Object.keys(uniq_ques_solved);
+		 var submissionsData={
+	 		uniq_ques_solved,
+	 		no_of_uniq_ques_solved:uniq_ques_solved.length,
+	 		accepted,
+	 		wrong_answer,
+	 		time_lim_exceed,
+	 		runtime_error,
+	 		mem_lim_exceed,
+	 		hacked,
+	 		compilation_error,
+	 	};
+		// console.log(submissionsData);
+		cb(null,submissionsData);
+	}
+
 
 }
 
